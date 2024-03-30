d005b81e0d8d7fea06bd87d8e7461a15521f30fa
kannan.c.mohan <kannanmohanklm@gmail.com>
2024-03-30 20:37:33 +0100

    feat: update cert-manager configuration27

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 ## Create new namespace "argocd"
-kubectl create namespace argocd --dry-run=client --output=yaml | kubectl apply -f -
+kubectl create namespace argocd --dry-run=client --output=yaml | kubectl apply -f - &&
 
 ## Install argocd
 helm template \
         --dependency-update --include-crds --namespace argocd \
         argocd ./k8s/cluster_apps/bootstrap/argocd \
-        --set argo-cd.configs.secret.argocdServerAdminPassword=$ARGOCD_ADMIN_PWD \
-        | kubectl apply -n argocd -f -
+        | kubectl apply -n argocd -f - &&
+
+## Wait for argocd server 
+echo 'Waiting for argocd server to start..' 
+kubectl wait -n argocd  --timeout=90s --for condition=Ready pod -l app.kubernetes.io/name=argocd-server &&
 
 ## create argocd resource Project and ApplicationSet
-helm template \
-        --namespace argocd \
-        argocd ./k8s/cluster_apps/bootstrap/root \
-        | kubectl apply -n argocd -f -
+helm template --namespace argocd argocd ./k8s/cluster_apps/bootstrap/root | kubectl apply -n argocd -f -
+
+# kubectl patch secret argocd-secret -n argocd -p '{"data": {"admin.password": null}}'
